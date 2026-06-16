a0b17a84f3fdeb3dbc301a4ea935981ed8acfdc8
leopold birungi <leopoldbirungi@gmail.com>
2026-06-16 13:26:30 +0300

    Add public admissions URLs

---
+++ apps/tenant/admissions/public_urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import public_views
+
+urlpatterns = [
+    path("", public_views.public_application_create, name="public_admissions_apply"),
+    path("submitted/<str:reference>/", public_views.public_application_success, name="public_admissions_success"),
+]
