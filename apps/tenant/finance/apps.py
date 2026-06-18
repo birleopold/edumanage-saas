723843d2c213a1c0c45fe396d2368ae509a66ff0
leopold birungi <leopoldbirungi@gmail.com>
2026-06-18 09:12:59 +0300

    Load finance integration models

--- apps/tenant/finance/apps.py
+++ apps/tenant/finance/apps.py
@@ -5,3 +5,9 @@ class FinanceConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.finance"
     label = "finance"
+
+    def ready(self):
+        try:
+            from . import accounting_models, integration_models, payment_gateway_models  # noqa: F401
+        except Exception:
+            pass
