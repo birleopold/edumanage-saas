1a962525f48a0d6e475612d3f1ecf2703be2fe30
leopold birungi <leopoldbirungi@gmail.com>
2026-06-18 10:42:11 +0300

    Register suspicious login alerts

--- apps/tenant/audit/admin.py
+++ apps/tenant/audit/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import AuditEvent, BackupJob, ConsentRecord, DataRetentionPolicy, ExportPermission, LoginHistory, UserTwoFactorSetting
+from .models import AuditEvent, BackupJob, ConsentRecord, DataRetentionPolicy, ExportPermission, LoginHistory, SuspiciousLoginAlert, UserTwoFactorSetting
 
 admin.site.register(AuditEvent)
 admin.site.register(LoginHistory)
@@ -9,3 +9,4 @@ admin.site.register(ExportPermission)
 admin.site.register(DataRetentionPolicy)
 admin.site.register(ConsentRecord)
 admin.site.register(BackupJob)
+admin.site.register(SuspiciousLoginAlert)
