0f6961111e85efb48d6b521bb9776eb34e852463
leopold birungi <leopoldbirungi@gmail.com>
2026-06-18 08:51:36 +0300

    Load analytics intelligence models

--- apps/tenant/analytics/apps.py
+++ apps/tenant/analytics/apps.py
@@ -5,3 +5,9 @@ class AnalyticsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.analytics"
     label = "analytics"
+
+    def ready(self):
+        try:
+            from . import intelligence_models  # noqa: F401
+        except Exception:
+            pass
