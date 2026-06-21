06f4d3440ea7fc21c6532ca8959c23e99f5f290a
leopold birungi <leopoldbirungi@gmail.com>
2026-06-21 22:36:10 +0300

    Add payment detail timeline view

---
+++ apps/tenant/finance/payment_history_views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import get_object_or_404, render
+
+from apps.tenant.portals.permissions import admin_portal_required
+
+from .models import Payment
+
+
+@admin_portal_required
+def payment_detail(request, pk: int):
+    payment = get_object_or_404(
+        Payment.objects.select_related(
+            "invoice",
+            "invoice__student",
+            "invoice__academic_year",
+            "invoice__academic_term",
+        ),
+        pk=pk,
+    )
+    return render(request, "portals/admin/finance/payment_detail.html", {"payment": payment, "invoice": payment.invoice})
