c17c0939ff5d36b7c0b92d1028f7a1739a54c6be
leopold birungi <leopoldbirungi@gmail.com>
2026-06-21 22:24:08 +0300

    Add admin device monitor route

--- apps/tenant/users/urls.py
+++ apps/tenant/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from . import views, setup_views
+from . import device_portal, setup_views, views
 
 urlpatterns = [
     path("", views.user_list, name="admin_users_list"),
+    path("devices/", device_portal.admin_device_monitor, name="admin_user_devices"),
     path("<int:pk>/roles/", views.user_roles_edit, name="admin_users_roles_edit"),
     path("setup/<str:token>/", setup_views.password_setup, name="password_setup"),
 ]
