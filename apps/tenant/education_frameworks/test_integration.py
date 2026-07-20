10f24a61884e8aac2ac910bfbed294106b23f8c8
leopold birungi <leopoldbirungi@gmail.com>
2026-07-20 21:03:59 +0300

    Cover MDD and external exam alias behavior

--- apps/tenant/education_frameworks/test_integration.py
+++ apps/tenant/education_frameworks/test_integration.py
@@ -3,14 +3,21 @@ from django.test import RequestFactory, TestCase
 
 from apps.tenant.orgsettings.models import Campus, OrganizationProfile
 
-from .integration import assessment_aliases_for_request, terminology_for_request
+from .integration import (
+    assessment_aliases_for_request,
+    external_exam_aliases_for_request,
+    framework_aliases_for_request,
+    terminology_for_request,
+)
 from .models import CampusEducationStage, EducationStage, FrameworkStage
 from .services import ensure_institution_profile, ensure_system_templates
 
 
 class EducationFrameworkIntegrationTests(TestCase):
     def setUp(self):
-        self.organization = OrganizationProfile.objects.create(name="Integration Test School")
+        self.organization = OrganizationProfile.objects.create(
+            name="Integration Test School"
+        )
         self.campus = Campus.objects.create(
             organization=self.organization,
             name="Main Campus",
@@ -25,10 +32,24 @@ class EducationFrameworkIntegrationTests(TestCase):
     def test_request_helpers_return_uganda_aliases_when_enabled(self):
         terms = terminology_for_request(self.request)
         aliases = assessment_aliases_for_request(self.request)
+        all_aliases = framework_aliases_for_request(self.request)
 
-        self.assertEqual(terms["external_exam"], "UNEB or External Exam")
-        self.assertEqual(aliases["BOT"], "Beginning of Term Test")
-        self.assertEqual(aliases["EOT"], "End of Term Examination")
+        self.assertEqual(
+            terms["external_exam"],
+            "UNEB or External Exam",
+        )
+        self.assertEqual(
+            aliases["BOT"],
+            "Beginning of Term Test",
+        )
+        self.assertEqual(
+            aliases["EOT"],
+            "End of Term Examination",
+        )
+        self.assertEqual(
+            all_aliases["MDD"],
+            "Music, Dance and Drama",
+        )
 
     def test_template_tag_can_be_adopted_without_changing_existing_models(self):
         primary = self.stages[EducationStage.PRIMARY]
@@ -44,23 +65,52 @@ class EducationFrameworkIntegrationTests(TestCase):
             academic_period_type=EducationStage.PERIOD_TERM,
         )
         self.profile.terminology = {"learner": "Pupil"}
-        self.profile.save(update_fields=["terminology", "updated_at"])
+        self.profile.save(
+            update_fields=["terminology", "updated_at"]
+        )
         template = Template(
-            "{% load education_terms %}{% education_term 'learner' %} | "
-            "{% education_alias 'MOT' %}"
+            "{% load education_terms %}"
+            "{% education_term 'learner' %} | "
+            "{% education_alias 'MOT' %} | "
+            "{% education_alias 'MDD' %}"
         )
 
-        rendered = template.render(Context({"request": self.request}))
+        rendered = template.render(
+            Context({"request": self.request})
+        )
 
-        self.assertEqual(rendered, "Pupil | Mid-Term Test")
+        self.assertEqual(
+            rendered,
+            "Pupil | Mid-Term Test | Music, Dance and Drama",
+        )
+
+    def test_external_exam_aliases_are_deduplicated(self):
+        self.profile.settings = {
+            "external_exam_aliases": [
+                "UCE",
+                "Cambridge",
+                "Cambridge",
+            ]
+        }
+        self.profile.save(update_fields=["settings", "updated_at"])
+
+        aliases = external_exam_aliases_for_request(self.request)
+
+        self.assertEqual(
+            aliases,
+            ["PLE", "UCE", "UACE", "UNEB", "Cambridge"],
+        )
 
     def test_local_aliases_are_disabled_in_neutral_mode(self):
         self.profile.use_local_terminology = False
-        self.profile.save(update_fields=["use_local_terminology", "updated_at"])
+        self.profile.save(
+            update_fields=["use_local_terminology", "updated_at"]
+        )
 
-        aliases = assessment_aliases_for_request(self.request)
+        aliases = framework_aliases_for_request(self.request)
         template = Template(
-            "{% load education_terms %}{% education_alias 'BOT' 'Diagnostic Assessment' %}"
+            "{% load education_terms %}"
+            "{% education_alias 'BOT' 'Diagnostic Assessment' %}"
         )
 
         self.assertEqual(aliases, {})
