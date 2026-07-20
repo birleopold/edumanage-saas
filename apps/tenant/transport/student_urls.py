e0e502eca96d927443db3e4081073480bad99f8c
leopold birungi <leopoldbirungi@gmail.com>
2026-07-20 19:29:22 +0300

    Add student transport detail route

--- apps/tenant/transport/student_urls.py
+++ apps/tenant/transport/student_urls.py
@@ -4,4 +4,9 @@ from . import student_views
 
 urlpatterns = [
     path("", student_views.my_transport, name="student_transport_home"),
+    path(
+        "assignments/<int:pk>/",
+        student_views.assignment_detail,
+        name="student_transport_assignment_detail",
+    ),
 ]
