77e6ed9e6b7ed8165c1c9cf9a9bbe17c72a9dc3d
leopold birungi <leopoldbirungi@gmail.com>
2026-07-20 20:09:58 +0300

    Add education frameworks app config

---
+++ apps/tenant/education_frameworks/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class EducationFrameworksConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.tenant.education_frameworks"
+    verbose_name = "Education Frameworks"
