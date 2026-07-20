d4377e0ca1535ab79d55c4d4d661b0ea3350e3d7
leopold birungi <leopoldbirungi@gmail.com>
2026-07-20 17:45:09 +0300

    Use database-enabled platform dashboard render test

--- apps/public/tenants/test_platform_dashboard_template.py
+++ apps/public/tenants/test_platform_dashboard_template.py
@@ -2,12 +2,12 @@ from types import SimpleNamespace
 
 from django.contrib.auth.models import AnonymousUser
 from django.template.loader import render_to_string
-from django.test import RequestFactory, SimpleTestCase, override_settings
+from django.test import RequestFactory, TestCase, override_settings
 from django.utils import timezone
 
 
 @override_settings(ROOT_URLCONF="config.public_urls")
-class PlatformDashboardTemplateTests(SimpleTestCase):
+class PlatformDashboardTemplateTests(TestCase):
     def test_system_activity_without_actor_or_tenant_renders_safely(self):
         request = RequestFactory().get("/platform/")
         request.user = AnonymousUser()
