8a59571fb24c7752060c09c608380ed7bc0e7141
leopold birungi <leopoldbirungi@gmail.com>
2026-07-20 18:54:04 +0300

    Activate temporary password compatibility at startup

--- apps/tenant/users/apps.py
+++ apps/tenant/users/apps.py
@@ -5,3 +5,8 @@ class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.users"
     label = "users"
+
+    def ready(self):
+        from .passwords import install_user_manager_password_compatibility
+
+        install_user_manager_password_compatibility()
