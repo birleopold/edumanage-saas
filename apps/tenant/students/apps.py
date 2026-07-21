e086e3516dee24b223912b5781bd24c01ac73f02
leopold birungi <leopoldbirungi@gmail.com>
2026-07-21 21:40:34 +0300

    Load student identity synchronization signals

--- apps/tenant/students/apps.py
+++ apps/tenant/students/apps.py
@@ -5,3 +5,6 @@ class StudentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.students"
     label = "students"
+
+    def ready(self):
+        from . import signals  # noqa: F401
