fd565e54121b4cdbd0568d0dfccc38ca534b216e
leopold birungi <leopoldbirungi@gmail.com>
2026-07-21 15:03:12 +0300

    Register Phase 8 activity models

--- apps/tenant/activities/apps.py
+++ apps/tenant/activities/apps.py
@@ -2,7 +2,10 @@ from django.apps import AppConfig
 
 
 class ActivitiesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.tenant.activities'
-    label = 'activities'
-    verbose_name = 'Activities'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.tenant.activities"
+    label = "activities"
+    verbose_name = "Activities"
+
+    def ready(self):
+        from . import programme_models  # noqa: F401
