b23338d18f211da3d8bb7247193f532887c0f508
leopold birungi <leopoldbirungi@gmail.com>
2026-07-21 09:14:09 +0300

    Enable learning activity synchronization signals

--- apps/tenant/coursework/apps.py
+++ apps/tenant/coursework/apps.py
@@ -2,7 +2,10 @@ from django.apps import AppConfig
 
 
 class CourseworkConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.tenant.coursework'
-    label = 'coursework'
-    verbose_name = 'Coursework'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.tenant.coursework"
+    label = "coursework"
+    verbose_name = "Coursework"
+
+    def ready(self):
+        from . import signals  # noqa: F401
