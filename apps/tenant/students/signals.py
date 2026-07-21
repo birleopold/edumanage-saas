05d6063f15a7580ae94c0c6f99625af0b7446cc3
leopold birungi <leopoldbirungi@gmail.com>
2026-07-21 21:40:23 +0300

    Keep student login identity aligned with student records

---
+++ apps/tenant/students/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import StudentProfile
+from .services import sync_student_user_identity
+
+
+@receiver(post_save, sender=StudentProfile)
+def synchronize_student_login_identity(sender, instance, **kwargs):
+    """Mirror the authoritative student identity into the linked login user."""
+
+    if instance.user_id:
+        sync_student_user_identity(instance)
