7792f5e609e2cfdc4b29ca961a0b6184c5877378
leopold birungi <leopoldbirungi@gmail.com>
2026-07-22 22:27:20 +0300

    Register Phase 2 assessment policies

--- apps/tenant/assessments/apps.py
+++ apps/tenant/assessments/apps.py
@@ -5,3 +5,6 @@ class AssessmentsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.assessments"
     label = "assessments"
+
+    def ready(self):
+        from . import policy_models, policy_signals  # noqa: F401
