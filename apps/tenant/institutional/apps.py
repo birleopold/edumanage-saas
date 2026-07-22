6b9ea04a91eff772f07e538259a2bf007bece4c8
leopold birungi <leopoldbirungi@gmail.com>
2026-07-22 22:56:35 +0300

    Register tertiary academic records

--- apps/tenant/institutional/apps.py
+++ apps/tenant/institutional/apps.py
@@ -5,3 +5,6 @@ class InstitutionalConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.institutional"
     verbose_name = "Institutional Education Operations"
+
+    def ready(self):
+        from . import academic_records  # noqa: F401
