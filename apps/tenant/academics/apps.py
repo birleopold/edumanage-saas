9a6107e8da0eac4eba6ecd83c409a674772f5c30
leopold birungi <leopoldbirungi@gmail.com>
2026-07-22 22:59:24 +0300

    Register subject-role policies

--- apps/tenant/academics/apps.py
+++ apps/tenant/academics/apps.py
@@ -5,3 +5,6 @@ class AcademicsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.tenant.academics"
     label = "academics"
+
+    def ready(self):
+        from . import pathway_extension_signals, pathway_extensions  # noqa: F401
