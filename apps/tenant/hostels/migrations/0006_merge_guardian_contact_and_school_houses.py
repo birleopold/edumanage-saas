0487d244ffa0d6e2bbc282eee56ff15388a49ac6
leopold birungi <leopoldbirungi@gmail.com>
2026-07-23 01:15:37 +0300

    Merge parallel hostels migration branches

---
+++ apps/tenant/hostels/migrations/0006_merge_guardian_contact_and_school_houses.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("hostels", "0005_guardiancontactlog_parent"),
+        ("hostels", "0005_school_houses_and_duty_rosters"),
+    ]
+
+    operations = []
